e10bf3731fc6adac2368a8cedc256fd35d56493b
Trojplosnik <stormtrooper3711@mail.ru>
2023-12-20 13:26:28 +0500

    add second picture

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ known_pixels = list()
 user_pixels = list()
 
 
-# def find_center(image: cv2.typing.MatLike) -> Pixel:
-#     height, width = image.shape[:2]
-#
-#     center_x = width // 2
-#     center_y = height // 2
-#
-#     return Pixel(center_x, center_y)
+def find_center(image: cv2.typing.MatLike) -> Pixel:
+    height, width = image.shape[:2]
+
+    center_x = width // 2
+    center_y = height // 2
+
+    return Pixel(center_x, center_y)
 
 
 def set_known_pixels(event, x, y, flags, param):
@@ -30,38 +30,32 @@ def get_user_pixels(event, x, y, flags, param):
         cv2.destroyAllWindows()
 
 
-def find_F(p1: Pixel, p2: Pixel, g: AccelerationDimensions, focal_length: float) -> float:
-    a = (g.x ** 2 + g.y ** 2) * (p1.x - p2.x) ** 2 + \
-        (g.y ** 2 + g.z ** 2) * (p1.y - p2.y) ** 2 + \
-        2 * g.x * g.y * (p2.x - p1.x) ** 2 * (p2.y - p1.y) ** 2
-    b = 2 * g.z * (p2.x * p1.y - p1.x * p2.y) \
-        * (g.y * (p1.x - p2.y) + g.x * (p2.y - p1.y))
-    c = (g.x ** 2 + g.y ** 2) * (p2.x * p1.y - p1.x * p2.y) ** 2
-    d = g.z ** 2
-    e = -(g.x * g.z * p2.x + g.y * g.z * p2.y
-          + g.x * g.z * p1.x + g.y * g.z * p1.y)
-    f = g.x ** 2 * p1.x * p1.y + g.x * g.y * p2.x * p1.y + \
-        g.x * g.y * p1.x * p2.y + g.y ** 2 * p1.y * p2.y
-    return math.sqrt(a * (focal_length ** 2) + b * focal_length + c) \
-        / abs(d * (focal_length ** 2) + e * focal_length + f)
-
-
-# def focal_length_prediction(image: cv2.typing.MatLike) -> float:
-#     return image.shape[0] / (2 * math.tan(0.5 * math.radians(60)))
+def focal_length_prediction(image: cv2.typing.MatLike) -> float:
+    return image.shape[0] / (2 * math.tan(0.5 * math.radians(60)))
 
 
 def find_depth(p1: Pixel, total: Total) -> float:
-    p0 = Pixel()
-    return (total.find_F(p0, p1) / total.known_F) * total.known_real_distance
+    return (total.find_F(total.center_pixel, p1) / total.known_F) \
+        * total.known_real_distance
+
+
+def find_ground_distance(p1: Pixel, p2: Pixel, total: Total) -> float:
+    return (total.find_F(p1, p2) / total.known_F) \
+        * total.known_real_distance
+
+
+def focal_length(p1: Pixel, p2: Pixel, total: Total) -> float:
+    return (total.find_F(p1, p2) / total.known_F) \
+        * total.known_real_distance
 
 
 def main():
-    image_path = "images/1.jpg"
+    image_path = "images/2.jpg"
     image = cv2.imread(image_path)
     known_real_distance = float(input())
-    acceleration_dimensions =\
+    acceleration_dimensions = \
         AccelerationDimensions(float(input()), float(input()), float(input()))
-    # center_coordinates = find_center(image)
+    center_coordinates = find_center(image)
     focal_length = float(input())
 
     cv2.namedWindow('image')
@@ -80,11 +74,29 @@ def main():
 
     total = Total(acceleration_dimensions,
                   focal_length, known_real_distance,
-                  known_pixels[0], known_pixels[1])
+                  center_coordinates,
+                  known_pixels)
 
-    print(center_coordinates)
-    print(known_pixels)
-    print(user_pixels)
+    map(total.convert_coordinates, user_pixels)
+    # 57.73919531315446
+    # 30.201450678085976
+    # 51.80754528860309
+
+    # 59.01528445072243
+    # 29.14370495143736
+    # 46.35700767563264
+    # 39.381173794287264
+
+# 60
+# -0.02
+# 0.66
+# -0.32
+# 2300
+
+    print(find_ground_distance(user_pixels[0], user_pixels[1], total))
+
+    for pixel in user_pixels:
+        print(find_depth(pixel, total))
 
 
 if __name__ == "__main__":
--- total.py
+++ total.py
@@ -1,28 +1,83 @@
 import math
 from structures import Pixel, AccelerationDimensions
 
+import numpy
+
 
 class Total:
     def __init__(self, acceleration_dimensions: AccelerationDimensions,
                  focal_length: float, known_real_distance: float,
-                 p1_known: Pixel, p2_known: Pixel):
+                center_pixel: Pixel, known_pixels: list[Pixel]):
         self.known_real_distance = known_real_distance
+        self.center_pixel = center_pixel
         self.g = acceleration_dimensions
         self.focal_length = focal_length
-        self.known_F =  self.find_F(p1_known, p2_known)
-
+        # self.focal_length = self.find_focal_length(focal_length_prediction,
+        #                                            *known_real_distance,
+        #                                            *known_pixels)
+        self.known_F = self.find_F(self.convert_coordinates(known_pixels[0]),
+                                   self.convert_coordinates(known_pixels[1]))
 
     def find_F(self, p1: Pixel, p2: Pixel) -> float:
-        a = (self.g.x ** 2 + self.g.y ** 2) * (p1.x - p2.x) ** 2 + \
-            (self.g.y ** 2 + self.g.z ** 2) * (p1.y - p2.y) ** 2 + \
-            2 * self.g.x * self.g.y * (p2.x - p1.x) ** 2 * (p2.y - p1.y) ** 2
-        b = 2 * self.g.z * (p2.x * p1.y - p1.x * p2.y) \
-            * (self.g.y * (p1.x - p2.y) + self.g.x * (p2.y - p1.y))
-        c = (self.g.x ** 2 + self.g.y ** 2) * (p2.x * p1.y - p1.x * p2.y) ** 2
-        d = self.g.z ** 2
-        e = -(self.g.x * self.g.z * p2.x + self.g.y * self.g.z * p2.y
-              + self.g.x * self.g.z * p1.x + self.g.y * self.g.z * p1.y)
-        f = self.g.x ** 2 * p1.x * p1.y + self.g.x * self.g.y * p2.x * p1.y + \
-            self.g.x * self.g.y * p1.x * p2.y + self.g.y ** 2 * p1.y * p2.y
+        a = self.a(p1, p2)
+        b = self.b(p1, p2)
+        c = self.c(p1, p2)
+        d = self.d()
+        e = self.e(p1, p2)
+        f = self.f(p1, p2)
         return math.sqrt(a * (self.focal_length ** 2) + b * self.focal_length + c) \
             / abs(d * (self.focal_length ** 2) + e * self.focal_length + f)
+
+    def convert_coordinates(self, p: Pixel):
+        return Pixel(p.y - self.center_pixel.y, self.center_pixel.x - p.x)
+
+
+    # def find_focal_length(self, focal_length_prediction: float,
+    #                       first_known_real_distance: float,
+    #                         second_known_real_distance: float,
+    #             p1: Pixel, p2: Pixel, q1: Pixel, q2: Pixel) -> float:
+    #     k = (first_known_real_distance / second_known_real_distance) ** 2
+    #     a1 = self.a(p1, p2)
+    #     b1 = self.b(p1, p2)
+    #     c1 = self.c(p1, p2)
+    #     d1 = self.d()
+    #     e1 = self.e(p1, p2)
+    #     f1 = self.f(p1, p2)
+    #     a2 = self.a(q1, q2)
+    #     b2 = self.b(q1, q2)
+    #     c2 = self.c(q1, q2)
+    #     d2 = self.d()
+    #     e2 = self.e(q1, q2)
+    #     f2 = self.f(q1, q2)
+    #     coefficients = [k * a2 * d1 ** 2 - a1 * d2 ** 2,
+    #                      2 * k * a2 * d1 * e1 - 2 * a1 * d2 * e2,
+    #                     (2 * k * a2 * d1 * f1 + k * a2 * e1 ** 2)
+    #                     - (2 * a1 * d2 * f2 + a1 * e2 ** 2),
+    #                     2 * k * a2 * e1 * f1 - 2 * a1 * e2 * f2,
+    #                     k * a2 * f1 ** 2 - a1 * f2 ** 2]
+    #     roots = numpy.roots(coefficients).tolist()
+    #     nearest = min(roots, key=lambda x: abs(x - focal_length_prediction))
+    #     return nearest
+
+    def a(self, p1: Pixel, p2: Pixel) -> float:
+        return (self.g.x ** 2 + self.g.y ** 2) * (p1.x - p2.x) ** 2 + \
+            (self.g.y ** 2 + self.g.z ** 2) * (p1.y - p2.y) ** 2 + \
+            2 * self.g.x * self.g.y * (p2.x - p1.x) * (p2.y - p1.y)
+
+    def b(self, p1: Pixel, p2: Pixel) -> float:
+        return 2 * self.g.z * (p2.x * p1.y - p1.x * p2.y) \
+            * (self.g.y * (p1.x - p2.x) + self.g.x * (p2.y - p1.y))
+
+    def c(self, p1: Pixel, p2: Pixel) -> float:
+        return (self.g.x ** 2 + self.g.y ** 2) * (p2.x * p1.y - p1.x * p2.y) ** 2
+
+    def d(self) -> float:
+        return self.g.z ** 2
+
+    def e(self, p1: Pixel, p2: Pixel) -> float:
+        return -(self.g.x * self.g.z * p2.x + self.g.y * self.g.z * p2.y
+              + self.g.x * self.g.z * p1.x + self.g.y * self.g.z * p1.y)
+
+    def f(self, p1: Pixel, p2: Pixel) -> float:
+        return self.g.x ** 2 * p1.x * p1.y + self.g.x * self.g.y * p2.x * p1.y + \
+            self.g.x * self.g.y * p1.x * p2.y + self.g.y ** 2 * p1.y * p2.y
